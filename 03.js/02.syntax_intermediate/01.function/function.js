9d6fde1529a89da96deb3df0e4e4ec177762781b
waitzero <magic2eye3@gmail.com>
2022-07-14 10:02:00 +0900

    자바스크립트 함수

--- 03.js/02.syntax_intermediate/01.function/function.js
+++ 03.js/02.syntax_intermediate/01.function/function.js
@@ -10,4 +10,16 @@ function add(a, b) {
 }
 
 //함수호출(call)
+// const addResult = add(2,3);
+// console.log(addResult);
+
 add(2, 3);
+
+// 두 수의 곱셈 결과를 변환하는 함수, mu;tiply
+function multiply(a, b) {
+  //함수 선언부
+  //그 외 다른 코드들, 함수 구현부(몸체)
+  return a * b;
+}
+
+console.log(multiply(2, 3));
