1d714817faf25ef68f71ac21142115b51c19c33f
waitzero <magic2eye3@gmail.com>
2022-07-14 13:59:11 +0900

    callback

--- 03.js/02.syntax_intermediate/01.function/03.callback.js
+++ 03.js/02.syntax_intermediate/01.function/03.callback.js
@@ -1,20 +1,38 @@
-//콜백 함수 =기본
-function greeting(name) {
-  console.log(`Hello ${name}`);
-} //$=탬플랏 리터럴 문법
+// // 콜백 함수 - 기본
+// function greeting(name) {
+//   console.log(`Hello ${name}`);
+// }
 
-function userInput(name, callbackFN) {
-  //const callbackFn = greeting
-  callbackFN(name); //greeting('Yoo')와 같음
-}
-//userInput 함수의 인자값으로 greetring 함수를 전달
-userInput("Yoo", greeting); //값으로 전달하는거니까 함수이름이 아닌 값으로 작성=>()필요없음
+// function userInput(name, callbackFn) {
+//   //눈에 보이지 않는 코드
+//   // const name = 'Yoo';
+//   // const callbackFn = greeting
 
-function waitCoupang(pkg, callbackFN) {
-  console.log(`쿠팡에서 ${pkg}가 도착했다.`);
-  callbackFN();
-}
-function bringToNeighbor() {
-  console.log(`옆집아주머니에게 전달완료`);
-}
-waitCoupang("사과", bringToNeighbor);
+//   callbackFn(name); // greeting('Yoo');와 같음.
+// }
+
+// // userInput함수의 인자값으로 greeting 함수를 전달
+// userInput("Yoo", greeting);
+
+// function waitCoupang(pkg, callback) {
+//   console.log(`쿠팡에서 ${pkg}가 도착했다.`);
+//   callback();
+// }
+
+// function bringItToNeighbor() {
+//   console.log("옆집 아주머니에게 전달 완료!");
+// }
+
+// waitCoupang("사과", bringItToNeighbor);
+
+const button = document.querySelector("button");
+
+// button.addEventListener("click", function () {
+//   console.log("button clicked!");
+// });
+
+const buttonFn = function () {
+  console.log("button clicked!");
+};
+
+// button.addEventListener("click", buttonFn);
